e3b276af862f6d389ac4be2f514d7b6084901e50
Faiz <faizhassan550@gmail.com>
2024-03-15 16:36:24 +0500

    Initial Commit

---
+++ swiftQRCodeScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftQRCodeScanner
+//
+//  Created by Faiz Ul Hassan on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
